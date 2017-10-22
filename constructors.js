694a4696c72045841d433080b67e8df5fdd76dee
alexdecycive <alex@decycive.com>
2017-10-22 13:03:37 -0400

    hit and miss working for letters chosen. Need to build game tallys

--- constructors.js
+++ constructors.js
@@ -2,11 +2,12 @@
 
 //List of words to be played
 var wordslistA = [
-	{	name: 'Jumbo', letters: [10, 21, 13, 2, 15]	},
-	{	name: 'Dingo', letters: [4, 9, 14, 7, 15] },
-	{	name: 'Dizzy', letters: [4, 9, 26, 26, 25] },
-	{	name: 'Plate', letters: [16, 12, 1, 20, 5] },
-	{	name: 'Tizzy', letters: [20, 9, 26, 26, 25] }
+	{	name: 'Jumbo', letters: ['j', 'u', 'm', 'b', 'o'] },
+	{	name: 'Dingo', letters: ['d', 'i', 'n', 'g', 'o'] },
+	{	name: 'Dizzy', letters: ['d', 'i', 'z', 'z', 'y'] },
+	{	name: 'Plate', letters: ['p', 'l', 'a', 't', 'e'] },
+	{	name: 'Tizzy', letters: ['t', 'i', 'z', 'z', 'y'] },
+	{   name: 'Testo', letters: ['t', 'e', 's', 't', 'o'] }
 	];
 
 
@@ -14,13 +15,13 @@ var wordslistA = [
 var randomnumber = Math.floor((Math.random() * 4) + 1);
 //console.log("Your random number is: " + randomnumber);
 
-var wordo = wordslistA[randomnumber].name;
+var word = wordslistA[randomnumber].name;
 //console.log("Word is: " + wordo);
 
 var letters = wordslistA[randomnumber].letters;
 //console.log("Letters are: " + letters);
 
-module.exports = { letters, wordo };
+module.exports = { letters, word };
 
 
 
--- hangman.js
+++ hangman.js
@@ -4,19 +4,6 @@ console.log("|||   Finding a criminal to hang    |||");
 console.log("|||   Scentencing him to death....  |||");
 console.log("|||   See if you can save him..     |||");
 console.log("---------------------------------------");
-
-//DELAY
-console.log("Please wait...");
-/*
-const delay = require('delay');
-delay(5000)
-  .then(() => {
-          // Executed after 200 milliseconds 
-          console.log("READY!");
-      });
-*/
-
-
 //Explain the game
 console.log("------------- HOW IT WORKS ------------");
 console.log("|||   Pick a letter and type it.    |||");
@@ -26,24 +13,24 @@ console.log("|||   Keep going until you win      |||");
 console.log("|||   or lose.                      |||");
 console.log("---------------------------------------");
 
+
 //Set packages
 var fs = require("fs");
 var request = require("request");
 var prompt = require("prompt");
+var date = require('date-and-time');
+var now = new Date();
 
+
+//Pull in constructor functions and actions
 var constructors = require('./constructors.js');
-//console.log("Constructors:" + constructors);
-var wordselected = constructors.word;
 console.log("The word for play has been selected.");
 console.log(" ");
 console.log("MAKE YOUR FIRST GUESS: __ __ __ __ __ ");
 
 
-var date = require('date-and-time');
-var now = new Date();
-
-//Log the start of the game
-fs.appendFile('log.txt', "HANGMAN has been run at: " + date.format(now, 'YYYY/MM/DD HH:mm:ss')  + " and the word selected is " + wordselected + ",", function(err) {
+//Log the start of the game including the word chosen
+fs.appendFile('log.txt', "HANGMAN has been run at: " + date.format(now, 'YYYY/MM/DD HH:mm:ss')  + " and the word selected is " + playword + ",", function(err) {
         if (err) {
         console.log(err);
         }
@@ -55,10 +42,11 @@ fs.appendFile('log.txt', "HANGMAN has been run at: " + date.format(now, 'YYYY/MM
 // GAME FUNCTIONS ----------------------------------------
 
 // Create a WORD
-console.log("Word is: " + constructors.wordo);
-
+console.log("Word is: " + constructors.word);
+var playword = constructors.word;
 // Create letters array
 console.log("Letters are: " + constructors.letters);
+var playletters = constructors.letters;
 
 
 //PROMPT - Ask the user what letter they want to play. 
@@ -68,13 +56,41 @@ console.log("Choose a letter:");
 prompt.get(['letter'], function (err, result) {
   // Log the results. 
   console.log(' --> You chose: ' + result.letter);
+  //convert to lowercase
+  var lowercaseinput = result.letter.toLowerCase();
+  console.log(' --> In lowercase: ' + lowercaseinput);
+
+  //Log the input
     fs.appendFile('log.txt', "The command was: " + result.letter + " logged at " + date.format(now, 'YYYY/MM/DD HH:mm:ss') + ",", function(err) {
       if (err) {
       console.log(err);
       }
     });
 
-    //Compare the letter chosen to the WORD
+    for (i=0; i < 6; i++) {
+      if (lowercaseinput === constructors.letters[i]) {
+      console.log("Hit!");
+      // Add to a counter of hits
+      /////////////////////////
+
+      }
+        else console.log("Miss!")
+          //Add to a counter of misses
+        ////////////////////////
+    }
+
+
+//You won function
+////////////////////////
+
+
+//You lose function
+//////////////////////////
+
+
+//Start over function
+////////////////////////////////
+
 
 
 //Closing the prompt
